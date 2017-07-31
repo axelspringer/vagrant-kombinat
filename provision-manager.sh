e3098e8dd91cc5722e7c961d4b03bbb3b93789c2
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-07-31 13:13:08 +0200

    ~ minor fix to networks

--- provision-manager.sh
+++ provision-manager.sh
@@ -5,8 +5,8 @@ MANAGER_IP=$1
 if which docker >/dev/null 2>&1; then
 	docker swarm init --listen-addr ${MANAGER_IP}:2377 --advertise-addr ${MANAGER_IP}
 	docker swarm join-token -q worker > /home/core/share/worker_token
-    docker network create --driver overlay --opt encrypted mesos
-	docker network create --driver overlay proxy
+    docker network create --driver overlay --opt encrypted --attachable mesos
+	docker network create --driver overlay --opt encrypted --attachable proxy
 	docker service create --detach=false --restart-delay 30s --restart-condition on-failure --name watchtower --mode global --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock centurylink/watchtower --cleanup
 	docker service create --container-label mesos=master -d -u root -p mode=host,target=8181,published=8181 --constraint 'node.role == manager' --network mesos --name exhibitor --mode global --restart-delay 30s --restart-condition any pixelmilk/exhibitor:1.5.6-3.4.10
     docker service create --container-label mesos=master -d -p mode=host,target=5050,published=5050 -e MESOS_ADVERTISE_IP=${MANAGER_IP} -e MESOS_PORT=5050 -e MESOS_ZK=zk://exhibitor:2181/mesos -e MESOS_QUORUM=1 -e MESOS_REGISTRY=in_memory --restart-delay 30s --constraint 'node.role == manager' --network mesos --name mesos-master --mode global --restart-condition any pixelmilk/mesos-master:1.3.0-2.0.3
